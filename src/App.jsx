dda5af2ac6b58445ab39e51239c3e2fc56d7a1b8
brajamohamed <b.rajamohamed@gmail.com>
2023-10-25 00:43:38 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="d-flex vh-100 align-items-center justify-content-center bg-dark text-warning">
+        <h2>Welcome to React</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
