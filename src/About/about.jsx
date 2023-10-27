72b5e98729d456de07703a3e76a74147e577c0e0
brajamohamed <b.rajamohamed@gmail.com>
2023-10-28 00:32:25 +0530

    corrected spelling mistake

--- src/About/about.jsx
+++ src/About/about.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 const About = () => {
   return (
     <div className="d-flex vh-100 flex-column justify-content-center align-items-center">
-      <h3>Flipkart by WallMart</h3>
+      <h3>Flipkart by WalMart</h3>
       <Link to="/products">Browse Products</Link>
     </div>
   );
