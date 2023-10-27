5fa0a52dc05970c8408254b9342822a236cee834
brajamohamed <b.rajamohamed@gmail.com>
2023-10-28 00:23:25 +0530

    fixed navbar bugs

--- src/Header/header.jsx
+++ src/Header/header.jsx
@@ -4,37 +4,40 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <nav className="navbar navbar-expand-lg bg-warning">
-      <Link to="/">
-        <h5 className="text-primary ms-5">Flipkart</h5>
-      </Link>
-      <button
-        className="navbar-toggler"
-        data-bs-toggle="collapse"
-        data-bs-target="#menu"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse ">
-        <ul className="navbar-nav navigation w-100" id="menu">
-          <li className="nav-item">
-            <Link to="/" className="nav-link text-white fw-bolder">
-              Home
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/about" className="nav-link text-white fw-bolder">
-              About
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/products" className="nav-link text-white fw-bolder">
-              Products
-            </Link>
-          </li>
-        </ul>
+    <div className="navbar navbar-expand-lg bg-warning">
+      <div className="container-fluid">
+        <Link to="/" className="navbar-brand">
+          <h5 className="text-primary ms-5">Flipkart</h5>
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#menu"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="menu">
+          <ul className="navbar-nav navigation w-100">
+            <li className="nav-item">
+              <Link to="/" className="nav-link text-white fw-bolder">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/about" className="nav-link text-white fw-bolder">
+                About
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/products" className="nav-link text-white fw-bolder">
+                Products
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
-    </nav>
+    </div>
   );
 };
 export default Header;
